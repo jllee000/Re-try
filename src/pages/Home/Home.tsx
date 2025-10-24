5245d5e3dcae88a686bd086604cbc22655c3c70d
jllee000 <jllee000@naver.com>
2025-10-25 02:17:26 +0900

    feat: geminai 추천 문구 연결

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,12 +1,13 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import UserCard from "./components/UserCard";
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import { useLocation } from "react-router-dom";
+
 const Home = () => {
     const location = useLocation();
     const queryParams = new URLSearchParams(location.search);
-    const username = queryParams.get("username") || "";
+    const username = queryParams.get("username") || ""; 
   const { data, isLoading, error } = useQuery({
     queryKey: ["users"],
     queryFn: async () => {
@@ -14,6 +15,28 @@ const Home = () => {
       return res.data;
     },
   });
+  const [message, setMessage] = useState<string>("");
+  useEffect(() => {
+      const fetchMessage = async () => {
+        try {
+         const res = await fetch("https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=AIzaSyDYLzC81gjW1KJDW4vqSGxAFXVumWaRjtw", {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({
+            contents: [{ parts: [{ text: `따뜻한 한 줄 응원문구로 딱 한개만 줘` }] }]
+          })
+        });
+        const data = await res.json();
+        setMessage(data.candidates?.[0]?.content?.parts?.[0]?.text || "오늘도 파이팅 💪");
+
+        } catch (err) {
+          console.error(err);
+          setMessage("응원문구를 불러오지 못했어요 😢");
+        }
+      };
+
+      fetchMessage();
+    }, [username]); 
 
   if (isLoading) return <div>로딩 중...</div>;
   if (error) return <div>에러 발생!</div>;
@@ -22,11 +45,11 @@ const Home = () => {
     <div className="flex flex-col justify-center items-center pt-[50px] relative">
        <div className="fixed top-[39px] w-[500px] h-[120px] mx-auto text-center z-10 bg-red-100 flex flex-col justify-center items-center ">
             <p>오늘 {username}에게</p>
-            <p className="text-[2rem]">화이팅!</p>
+            <p className="text-[1.5rem]">{message}</p>
        </div>
       <div className="relative pt-[120px] w-full mx-auto flex flex-col gap-4 justify-center items-center">
-            {data?.map((user: any) => (
-                <UserCard key={user.userKey} name={user.name} userKey={user.userKey} />
+            {data?.map((user: any,idx:number) => (
+                <UserCard key={idx} name={user.name} userKey={user.userKey} />
         ))}
       </div>
     </div>
